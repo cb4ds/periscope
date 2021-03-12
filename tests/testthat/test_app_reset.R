6654a272e8d5c0b63ba4018097246085d6634ac1
Mohammed Ali <mohammed@aggregate-genius.com>
2021-03-12 00:09:11 -0800

    reverted DESCRIPTION file changes

--- tests/testthat/test_app_reset.R
+++ tests/testthat/test_app_reset.R
@@ -7,27 +7,34 @@ test_that(".appResetButton", {
 
 test_that(".appReset - no reset button", {
     # there is no reset button on the UI for the app
-    testServer(.appReset, 
+    testServer(.appReset,
                {session$setInputs(resetPending = NULL)
                 expect_silent(.appReset)})
 })
 
 test_that(".appReset - reset button - no pending", {
     # there is no reset button on the UI for the app
-    suppressWarnings(testServer(.appReset, 
+    suppressWarnings(testServer(.appReset,
                {session$setInputs(resetButton = TRUE, resetPending = FALSE)
                 expect_silent(.appReset)}))
 })
 
 test_that(".appReset - no reset button - with pending", {
     # there is no reset button on the UI for the app
-    suppressWarnings(testServer(.appReset, 
+    suppressWarnings(testServer(.appReset,
                {session$setInputs(resetButton = FALSE, resetPending = TRUE)
                    expect_silent(.appReset)}))
 })
 
 test_that(".appReset - reset button - with pending", {
-    suppressWarnings(testServer(.appReset, 
+    suppressWarnings(testServer(.appReset,
                                 {session$setInputs(resetButton = TRUE, resetPending = TRUE)
                                  expect_silent(.appReset)}))
+})
+
+test_that(".appReset", {
+    expect_silent(.appReset(input = list(resetButton = TRUE, resetPending = FALSE),
+                                   output = list(), 
+                                   session = MockShinySession$setInputs(resetButton = TRUE, resetPending = FALSE),
+                                   logger = periscope:::fw_get_user_log()))
 })
