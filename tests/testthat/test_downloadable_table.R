8072b65e269b9211bae7e620a857ec3755057111
Mohammed Ali <mohammed@aggregate-genius.com>
2021-08-08 23:17:07 -0700

    add unit tests

--- tests/testthat/test_downloadable_table.R
+++ tests/testthat/test_downloadable_table.R
@@ -17,9 +17,6 @@ mydataRowIds <- function(){
     rownames(mtcars)
 }
 
-
-
-
 test_that("downloadableTable", {
     session <- MockShinySession$new()
     session$setInputs(dtableSingleSelect = TRUE)
@@ -35,4 +32,33 @@ test_that("downloadableTable", {
                                     downloaddatafxns = list(csv = data, tsv = data),
                                     tabledata = data,
                                     selection = mydataRowIds))
+})
+
+test_that("build_datatable_arguments", {
+    local_edition(3)
+    table_options <- list(rownames = FALSE,
+                          callback = "table.order([2, 'asc']).draw();",
+                          caption = " Very Important Information",
+                          colnames = c("Area", "Delta", "Increase"),
+                          filter = "bottom",
+                          width = "150px",
+                          height = "50px",
+                          extensions = 'Buttons',
+                          plugins = 'natural',
+                          editable = TRUE,
+                          order = list(list(2, 'asc'), list(3, 'desc')))
+    expect_snapshot(build_datatable_arguments(table_options))
+})
+
+
+test_that("format_columns", {
+    local_edition(3)
+    set.seed(123)
+    dt <-  cbind(matrix(rnorm(60, 1e5, 1e6), 20), runif(20), rnorm(20, 100))
+    dt[, 1:3] = round(dt[, 1:3])
+    dt[, 4:5] = round(dt[, 4:5], 7)
+    colnames(dt) = head(LETTERS, ncol(dt))
+    expect_snapshot(format_columns(DT::datatable(dt), 
+                                   list(formatCurrency = list(columns = c("A", "C")),
+                                        formatPercentage = list(columns = c("D"), 2))))
 })
